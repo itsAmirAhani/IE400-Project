c297b0fb3ff944581f4bf0a302856799fbfcf819
AmirhosseinAhani <amir.ahani1377@gmail.com>
2025-05-04 11:59:25 +0300

    updated code

--- plot_omega_vs_dissimilarity.py
+++ plot_omega_vs_dissimilarity.py
@@ -2,11 +2,3 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('omega_vs_dissimilarity.csv')
-
-plt.figure()
-plt.plot(data['Omega'], data['Max_Dissimilarity'], marker='o')
-plt.xlabel('Omega (%)')
-plt.ylabel('Maximum Dissimilarity')
-plt.title('Omega vs Maximum Dissimilarity')
-plt.grid(True)
-plt.show()
